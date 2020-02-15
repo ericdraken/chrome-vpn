7c0137f4916db3acaab3b5f627026f8b4555ca16
Eric Draken <ericdraken@gmail.com>
2020-02-14 17:19:32 -0800

    Renamed an s6 service

--- root/app/reconnect.sh
+++ root/app/reconnect.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/with-contenv bash
+set -e
 
 echo "Restarting VPN service"
 # See: https://skarnet.org/software/s6/s6-svc.html
-s6-svc -r /var/run/s6/services/nordvpnd
+s6-svc -r /var/run/s6/services/openvpn
 
 exit 0
